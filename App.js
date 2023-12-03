17fe1c8d7c4f3606c1b3a9a69d7fb2f5230a0d65
Alexander Pataridze <s.pataridze@gmail.com>
2023-12-03 17:12:21 +0400

    chore: upgrade project

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import React from 'react';
-import { View } from 'react-native';
-import Navigator from './navigator';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Login from "./Login";
+import MorphSlider from "./MorphSlider";
+import Curve from "./Curve";
+import Menu from "./MenuThree";
+import BalloonSlider from "./BalloonSlider";
+import { useState } from "react";
+
+const divider = <View style={{ height: 10 }} />;
+
+const screens = [
+  {
+    name: "Temperature Control",
+    Component: MorphSlider,
+  },
+  {
+    name: "Login page",
+    Component: Login,
+  },
+  {
+    name: "Curved add to cart",
+    Component: Curve,
+  },
+  {
+    name: "3D Burger Menu",
+    Component: Menu,
+  },
+  {
+    name: "Balloon Slider",
+    Component: BalloonSlider,
+  },
+];
 
 export default function App() {
+  const [selectedScreen, setSelectedScreen] = useState(-1);
   return (
-    <View style={{ flex: 1 }}>
-      <Navigator />
+    <View style={styles.container}>
+      {selectedScreen === -1 &&
+        screens.map(({ name }, i) => (
+          <>
+            <Button title={name} onPress={() => setSelectedScreen(i)} />
+            {divider}
+          </>
+        ))}
+      {selectedScreen >= 0 && screens.map(({ Component }) => <Component />)}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
